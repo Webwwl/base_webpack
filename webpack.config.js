63bcfc3e197d17c411ded30c1c4f653f3783f370
wuwenliang.kt <wuwenliang.kt@bytedance.com>
2023-10-18 15:10:11 +0800

    feat: 增加 css less 类型文件支持

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,14 @@ const config = {
       {
         test: /\.jsx?$/,
         use: 'babel-loader'
+      },
+      {
+        test: /\.css?$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.less?$/,
+        use: ['style-loader', 'css-loader', 'less-loader']
       }
     ]
   },
